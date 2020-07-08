1a0b071cd7128f7d6f18c9e514e88a1afa1689c5
Stevezhuravel <Stevezhuravel@gmail.com>
2020-07-08 11:35:23 -0400

    basic js function like alert console.log prompt

---
+++ script.js
@@ -0,0 +1,6 @@
+// alert("Hello From js file");
+// stores it in a variable
+var userName = prompt("What is your name");
+// print a message and users the variable to store the name than print it 
+alert("nice to meet you," + userName)
+console.log("Also great to meet you, " + userName)
